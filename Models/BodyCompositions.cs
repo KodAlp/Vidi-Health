2316016d5bb32d066243e8d3f5e12030b63282ba
KodAlp <ilteralpokmen@gmail.com>
2025-09-07 19:23:07 +0300

    Added Files
    Body Composition and Measurements class have been added to repository.DietContext is also added and that class is for sql implementation

---
+++ Models/BodyCompositions.cs
@@ -0,0 +1,26 @@
+﻿namespace Vidi_Health.Models
+{
+    public class BodyCompositions
+    {
+        public double BodyFatPercentage { get; set; }
+        public double LeanBodyMass { get; set; } // in kg
+
+        public double FatMass { get; set; } // in kg
+
+        public DateTime CalculatedAt { get; set; } = DateTime.Now;
+
+        public double BMR { get; set; } // Basal Metabolic Rate
+        public double TDEE { get; set; } // Total Daily Energy Expenditure
+
+        public Calculation_type calculation { get; set; }
+
+        public int MeasurementId { get; set; }
+
+        public enum Calculation_type
+        {
+            NavyFormula = 0,
+            JacksonPollock3 = 1,
+            JacksonPollock7 = 2
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,14 +1,61 @@
 ﻿using System.ComponentModel.DataAnnotations;
+
 namespace Vidi_Health.Models
 {
     public class User
     {
         //Primary Key
         [Key]
-        public int Id  { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(80)] // Can you maximum 80 characters for the name
+        public string name { get; set; }
 
+        [Required]
+        public DateTime dateOfBirth { get; set; }
+
+        [Required]
+        public Gender gender { get; set; }
+        [Required]
+        public ActivityLevel activityLevel { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [Required]
+        public Ethnicity your_ethnicity { get; set; }
+
+        public List <Measurements> measurements { get; set; } = new();
+        public List <BodyCompositions> bodyCompositions { get; set; } = new();
+        public int age => DateTime.Now.Year - dateOfBirth.Year;
     }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public enum Ethnicity
+    {
+        Caucasian =0,
+        Asian =1,
+        African =2,
+        Hispanic =3,
+        MiddleEastern =4,
+        NativeAmerican =5,
+        PacificIslander =6,
+        SouthAsian =7,
+        EastAsian =8,
+        Mixed =9,
+    }
+    public enum ActivityLevel
+    {
+        Sedentary =0, //1.2 Little or no exercise
+        LightlyActive =1, //1.375 Light exercise/sports 1-3 days/week
+        ModeratelyActive =2, //1.55 Moderate exercise/sports 3-5 days/week
+        VeryActive =3, //1.725 Hard exercise/sports 6-7 days a week
+        SuperActive = 4 //1.9 Very hard exercise/sports & physical job or 2x training
+    }
+
+
 }
