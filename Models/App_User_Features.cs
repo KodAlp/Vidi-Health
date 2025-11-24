fbd5a942cc99565da85781450ba9a30d72799e2d
KodAlp <ilteralpokmen@gmail.com>
2025-11-24 14:41:31 +0300

    Files have ben concatenated , Now have to fix the Bağlams and start to create Database Connection

---
+++ Models/App_User_Features.cs
@@ -0,0 +1,152 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Vidi_Health.Models
+{
+    internal class App_User_Features
+    {
+        private static readonly List<The_Complex_Measurements> complex_Measurements= new();
+        public List<The_Measurements> Measurements { get; set; } = new();
+        public List<The_Complex_Measurements> complex_measurements { get; set; } = complex_Measurements;
+
+
+        public enum MeasurementType
+        {
+            NavyFormula = 0,        // Tape measure method
+            JacksonPollock3 = 1,    // 3-point caliper
+            JacksonPollock7 = 2     // 7-point caliper
+        }
+
+        public class The_User
+        {
+            [Key] //Account ID
+            public int Id { get; set; }
+
+            [Required,NotNull,MaxLength(45)]
+            public string Name { get; set; }
+
+
+            public DateTime AccountCreation {  get; set; }
+
+            [Required]
+            public string Mail_Adress { get; set; }
+
+
+        }
+
+        public class The_Personal_Info 
+        { 
+
+            public DateTime DateOfBirth { get; set; }        
+        
+            public int Age { get; set; }
+
+            public Ethnicity User_Ethnic {  get; set; }
+
+            public enum Gender
+            {
+                Woman =0,
+                Man =1,
+                Empty = 2,
+
+            }
+
+            public enum Ethnicity
+            {
+                Caucasian = 0,
+                Asian = 1,
+                African = 2,
+                Hispanic = 3,
+                MiddleEastern = 4,
+                NativeAmerican = 5,
+                PacificIslander = 6,
+                SouthAsian = 7,
+                EastAsian = 8,
+                Mixed = 9,
+            }
+
+            public enum ActivityLevel
+            {
+                Sedentary = 0, //1.2 Little or no exercise
+                LightlyActive = 1, //1.375 Light exercise/sports 1-3 days/week
+                ModeratelyActive = 2, //1.55 Moderate exercise/sports 3-5 days/week
+                VeryActive = 3, //1.725 Hard exercise/sports 6-7 days a week
+                SuperActive = 4 //1.9 Very hard exercise/sports & physical job or 2x training
+            }
+        }
+
+
+        public class The_Measurements 
+        {
+            public MeasurementType Type { get; set; }
+            //System.Half Floatin bir küçüğü, 2 byte ve onalık gösterim sağlayabiliyor
+            [Required,NotNull]
+            public float  Weight { set; get; }
+        
+            [Required, NotNull]
+            public float Height { set; get; }
+            
+            [Required, NotNull] 
+            public float Waist_Measure { set; get; }
+
+            [Required, NotNull]
+            public float Neck_Measure { set; get; }
+            
+            [Required, NotNull]
+            public float Hip_Measure { set; get; }
+
+            [Required, NotNull]
+            public float SkinfoldChest { set; get; }
+
+            [Required, NotNull]
+            public float SkinfoldAbdominal {get; set;}
+
+            [Required, NotNull]
+            public float Skinfoldthigh {  set; get; }
+
+            [Required, NotNull]
+            public float SkinfoldSuprailiac { set; get; }
+
+            [Required, NotNull]
+            public float SkinfoldSubscapular { set; get; }
+
+            public DateTime MeasureTime { set; get; }
+
+
+
+        }
+
+        public class The_Complex_Measurements
+        {
+            //Hesaplanan Yağ Oranı
+            public double BodyFatPercentage { get; set; }
+
+            //Yağsız Vücut Kitlesi (KG)
+            public double LeanBodyMass { get; set; }
+
+            //Yağ miktarı (KG)
+            public double FatMass { get; set; }
+
+
+            //Bazal Metabolizma Hızı / Basal Metabolic Rate
+            public double BMR { get; set; }
+
+            //Günlük gerekli enerji miktarı / Total Daily Energy Expenditure
+            public double TDEE { get; set; }
+
+            public Calculation_type Method { get; set; }
+
+            //Database access
+            public int MeasurementId { get; set; }
+
+            //Checkbox index taking
+            public enum Calculation_type
+            {
+                NavyFormula = 0,
+                JacksonPollock3 = 1,
+                JacksonPollock7 = 2
+            }
+
+        }
+    }
+}
---
+++ Services/Calculators.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vidi_Health.Models;
+namespace Vidi_Health.Services
+{
+    internal class Calculators
+    {
+        public class BmrCalculatorService : Interfaces.IBmrCalculatorService
+        {
+            [DllImport("MathEngine.dll")]
+            private static extern double MifflinStJeorMale(double weight, double height, int age);
+
+            [DllImport("MathEngine.dll")]
+            private static extern double MifflinStJeorFemale(double weight, double height, int age);
+
+
+            [DllImport("MathEngine.dll")]
+            private static extern double CunningHamFormula(double LeanbodyMass);
+
+            [DllImport("MathEngine.dll")]
+            private static extern double CalculateTDEE(double bmr, int activitylevel);
+
+
+            public double CalculateBMR(App_User_Features.The_User user, App_User_Features.The_Measurements measurement)
+            {
+                try
+                {
+                    if (measurement.Weight <= 0 || measurement.Height <= 0 || user.Age == -1)
+                        throw new ArgumentException("Please give proper measurements for the calculation!");
+
+                    if (user.Gender == Gender.Male)
+                    {
+                        double result = MifflinStJeorMale(measurement.Weight, measurement.Height, user.Age);
+                        return result;
+                    }
+                    else if (user.Gender == Gender.Female)
+                    {
+                        double result = MifflinStJeorFemale(measurement.Weight, measurement.Height, user.Age);
+                        return result;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Calculation Error please try again later.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new ArgumentException("Unexpected error in Bmmr calculation", e);
+                }
+            }
+
+            public double CalculateBMRWithBodyFat(App_User_Features.The_Measurements measurement, double bodyfat)
+            {
+                try
+                {
+                    double result;
+
+                    double Lean_body_mass = measurement.Weight - (measurement.Weight * bodyfat / 100);
+                    result = CunningHamFormula(Lean_body_mass);
+                    return result;
+
+                }
+                catch (Exception e)
+                {
+                    throw new ArgumentException("Unexpected variables please try again.", e);
+                }
+            }
+
+            public double CalculateTDEE(double bmr, ActivityLevel activityLevel)
+            {
+                try
+                {
+                    double result_bmr;
+                    result_bmr = CalculateTDEE(bmr, (int)activityLevel);
+                    return result_bmr;
+                }
+                catch (Exception e)
+                {
+                    throw new ArgumentException("Unexpected error on calculating the calories please try later", e);
+                }
+
+            }
+
+            public double CalculateCaloricNeeds(App_User_Features.The_Measurements measurement, App_User_Features.The_User user, double body_fatpercentage)
+            {
+                try
+                {
+                    if (body_fatpercentage != -1)
+                        return CalculateBMRWithBodyFat(measurement, body_fatpercentage);
+
+                    else
+                        return CalculateBMR(user, measurement);
+                }
+                catch
+                {
+                    throw new ArgumentException("Please choose the calculations correctly");
+                }
+            }
+
+        }
+
+        public class BodyFatCalculatorService : Interfaces.IBodyFatCalculatorService
+        {
+            [DllImport("MathEngine.dll")]
+            private static extern double NavyMale(double waist, double neck, double height);
+
+            [DllImport("MathEngine.dll")]
+            private static extern double NavyFemale(double waist, double neck, double hip, double height);
+
+            [DllImport("MathEngine.dll")]
+            private static extern double JP3Male(double chest, double abdomen, double thigh, int age);
+
+            [DllImport("MathEngine.dll")]
+            private static extern double JP3Female(double triceps, double abdomen, double suprailiac, int age);
+
+
+            [DllImport("MathEngine.dll")]
+            private static extern double JP7Male(double sum7points, int age);
+
+            [DllImport("MathEngine.dll")]
+            private static extern double JP7Female(double sum7points, int age);
+
+            public double CalculateBodyFat(App_User_Features.The_Measurements measurement, App_User_Features.The_User user)
+            {
+                double bf;
+                if (measurement.Type == MeasurementType.NavyFormula)
+                    bf = CalculateNavyFormula(measurement, user);
+                else if (measurement.Type == MeasurementType.JacksonPollock3)
+                    bf = CalculateJacksonPollock3(measurement, user);
+                else if (measurement.Type == MeasurementType.JacksonPollock7)
+                    bf = CalculateJacksonPollock7(measurement, user);
+                else
+                    throw new ArgumentException("Invalid measurement type");
+                return bf;
+            }
+
+            //C++ backend NavyFormula
+            public double CalculateNavyFormula(App_User_Features.The_Measurements measurement, App_User_Features.The_User user)
+            {
+                double result;
+                try
+                {
+                    if (!measurement.NeckCircumference.HasValue || !measurement.WaistCircumference.HasValue)
+                        throw new ArgumentException("For navy formula need waist and neck measurements!");
+
+                    if (user.Gender == Gender.Male)
+                    {
+                        result = NavyMale(measurement.WaistCircumference.Value,
+                            measurement.NeckCircumference.Value, measurement.Height);
+
+                    }
+                    else if (user.Gender == Gender.Female)
+                    {
+                        if (!measurement.HipCircumference.HasValue)
+                            throw new ArgumentException("For navy formula for femal need a proper hip measurement.");
+                        result = NavyFemale(measurement.WaistCircumference.Value,
+                            measurement.NeckCircumference.Value,
+                            measurement.HipCircumference.Value,
+                            measurement.Height
+                            );
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Gender is not defined, please choose a proper gender.");
+                    }
+
+                    return Math.Max(0, Math.Min(50, result));
+                }
+                catch
+                {
+
+                    return 0.0;//Must fill here about unexpected   errors.
+                }
+            }
+
+            //C++ backend JP3 Formula
+            public double CalculateJacksonPollock3(App_User_Features.The_Measurements measurement, App_User_Features.The_User user)
+            {
+                try
+                {
+
+                    if (user.Gender == Gender.Male)
+                    {
+                        double result;
+                        if (!measurement.ChestSkinfold.HasValue || !measurement.AbdominalSkinfold.HasValue ||
+                            !measurement.ThighSkinfold.HasValue || user.Age == -1)
+                            throw new ArgumentException("Please satisfy the measurements first.");
+
+
+                        result = JP3Male(measurement.ChestSkinfold.Value, measurement.AbdominalSkinfold.Value,
+                            measurement.ThighSkinfold.Value, user.Age);
+                        return result;
+                    }
+                    else if (user.Gender == Gender.Female)
+                    {
+                        double result;
+                        if (!measurement.TricepsSkinfold.HasValue || !measurement.AbdominalSkinfold.HasValue ||
+                           !measurement.SuprailiacSkinfold.HasValue || user.Age == -1)
+                            throw new ArgumentException("Please satisfy the measurements first.");
+
+                        result = JP3Female(measurement.TricepsSkinfold.Value, measurement.AbdominalSkinfold.Value,
+                            measurement.SuprailiacSkinfold.Value, user.Age);
+                        return result;
+
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Please Choose proper gender");
+                    }
+
+                }
+                catch
+                {
+                    throw new ArgumentException("Unexpected error while calculating bodyfat. Please try again later");
+                }
+            }
+
+            //C++ backend JP7 Formula
+            public double CalculateJacksonPollock7(App_User_Features.The_Measurements measurement, App_User_Features.The_User user)
+            {
+                try
+                {
+                    double result, jp7;
+                    if (!measurement.TricepsSkinfold.HasValue || !measurement.AbdominalSkinfold.HasValue ||
+                        !measurement.ThighSkinfold.HasValue || !measurement.SuprailiacSkinfold.HasValue ||
+                        !measurement.SubscapularSkinfold.HasValue || !measurement.MidaxillarySkinfold.HasValue
+     ||
+                        !measurement.ChestSkinfold.HasValue || user == null)
+
+                        throw new Exception("Please give proper measurements for calculation.");
+
+
+                    jp7 = measurement.TricepsSkinfold.Value + measurement.AbdominalSkinfold.Value + measurement.ThighSkinfold.Value +
+                        measurement.SuprailiacSkinfold.Value + measurement.SubscapularSkinfold.Value + measurement.MidaxillarySkinfold.Value
+                        + measurement.ChestSkinfold.Value;
+                    if (user.Gender == Gender.Female)
+                    {
+                        result = JP7Female(jp7, user.Age);
+                        return result;
+                    }
+                    else if (user.Gender == Gender.Male)
+                    {
+                        result = JP7Male(jp7, user.Age);
+                        return result;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Please choose proper gender");
+                    }
+                }
+                catch
+                {
+                    throw new ArgumentException("Unexpected error on calculation please try again later."); ;
+                }
+            }
+        }
+
+    }
+}
